1d7897ff1cb6903a49f79fd4dad42b8d27252ab1
nljubivaya <nastyshalubivaya@gmail.com>
2025-04-05 01:34:16 +0300

    add course 2

--- SEDO_Training/obj/Debug/net8.0/SEDO_Training.AssemblyInfo.cs
+++ SEDO_Training/obj/Debug/net8.0/SEDO_Training.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEDO_Training")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8fe97410073797e468b5ca00c456b2581bc6564")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d24bc98d36492ba359a5ee58d32fc84f1cf81e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEDO_Training")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEDO_Training")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
